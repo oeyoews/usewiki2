7a6dc9ce4c11dc6bf6518faf03737fa7b4a43d35
oeyoews <jyao4783@gmail.com>
2024-03-24 11:59:09 +0800

    update

--- entrypoints/popup/Popup.vue
+++ entrypoints/popup/Popup.vue
@@ -319,6 +319,15 @@ watch(port, (newValue) => {
           </ElIcon>
         </template>
         <div v-if="title">
+          <div class="flex items-center justify-center gap-2">
+            <h2>
+              <a :href="link" target="_blank" v-if="link">
+                <img :src="faviconUrl" class="rounded-full size-4" />
+              </a>
+              {{ title }}
+            </h2>
+          </div>
+          <el-divider border-style="dashed" />
           <article class="prose prose-gray max-w-none prose-sm dark:prose-invert">
             <div v-html="aihtml"></div>
           </article>
