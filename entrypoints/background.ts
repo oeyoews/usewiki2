c7ff69726aa4f822ede75aa17807e98c9e3136bf
oeyoews <jyao4783@gmail.com>
2024-04-03 20:38:41 +0800

    clean comment code

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -28,36 +28,4 @@ export default defineBackground(() => {
         .catch((error) => console.error(error));
     }
   });
-
-  // browser.runtime.onInstalled.addListener(async ({ reason }) => {
-  //   if (reason !== 'install') return;
-  //   await browser.tabs.create({
-  //     url: browser.runtime.getURL('/welcome.html'),
-  //     active: true,
-  //   });
-  // });
-  // browser.browserAction.setTitle({ title: 'Usewiki2' });
-  // browser.browserAction.setBadgeText({ text: 'Usewiki2' });
-  // chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  //   chrome.action.setBadgeText({ text: 'on' });
-  // });
-  /*   chrome.runtime.onInstalled.addListener(function () {
-    chrome.contextMenus.create({
-      id: 'tiddlywiki',
-      title: '添加到 TiddlyWiki(WIP)',
-      contexts: ['all'],
-    });
-  });
-
-  chrome.commands.onCommand.addListener(function (command) {
-    if (command === 'addtiddlywiki') {
-      // 处理快捷键被触发时的逻辑
-    }
-  });
-
-  chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    if (info.menuItemId === 'tiddlywiki') {
-      // 处理右键菜单点击事件的逻辑
-    }
-  }); */
 });
