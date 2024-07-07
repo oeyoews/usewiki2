444ed526876a1ccf53082b897367031150cf205c
oeyoews <jyao4783@gmail.com>
2024-07-07 11:05:33 +0800

    darkmode

--- entrypoints/sidepanel/Sidepanel.vue
+++ entrypoints/sidepanel/Sidepanel.vue
@@ -258,6 +258,57 @@ async function toggleDarkMode() {
   }
 }
 
+// const isAppearanceTransition =
+//   document.startViewTransition &&
+//   !window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+
+// /**
+//  * Credit to [@hooray](https://github.com/hooray)
+//  * @see https://github.com/vuejs/vitepress/pull/2347
+//  */
+// async function toggleDark(event?: MouseEvent) {
+//   const DARK = 'dark';
+//   if (!isAppearanceTransition || !event) {
+//     isDarkMode.value = !isDarkMode.value;
+//     await isDarkModeStorage.setValue(isDarkMode.value);
+//     return;
+//   }
+//   if (isDarkMode.value) {
+//     document.documentElement.classList.add(DARK);
+//   } else {
+//     document.documentElement.classList.remove(DARK);
+//   }
+//   const x = event.clientX;
+//   const y = event.clientY;
+//   const endRadius = Math.hypot(
+//     Math.max(x, innerWidth - x),
+//     Math.max(y, innerHeight - y)
+//   );
+//   // @ts-expect-error: Transition API
+//   const transition = document.startViewTransition(async () => {
+//     isDarkMode.value = !isDarkMode.value;
+//     await nextTick();
+//   });
+//   transition.ready.then(() => {
+//     const clipPath = [
+//       `circle(0px at ${x}px ${y}px)`,
+//       `circle(${endRadius}px at ${x}px ${y}px)`,
+//     ];
+//     document.documentElement.animate(
+//       {
+//         clipPath: isDarkMode.value ? [...clipPath].reverse() : clipPath,
+//       },
+//       {
+//         duration: 400,
+//         easing: 'ease-in',
+//         pseudoElement: isDarkMode.value
+//           ? '::view-transition-old(root)'
+//           : '::view-transition-new(root)',
+//       }
+//     );
+//   });
+// }
+
 const toggleInfoDialog = () => {
   infoDialogStatus.value = !infoDialogStatus.value;
 };
