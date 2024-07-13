0a1c8941b2b7c95a335739f366bebc21a88ab824
oeyoews <jyao4783@gmail.com>
2024-07-13 11:30:22 +0800

    update

--- entrypoints/sidepanel/Sidepanel.vue
+++ entrypoints/sidepanel/Sidepanel.vue
@@ -737,17 +737,17 @@ const toggleInfoDialog = () => {
     height: 7px;
   }
   /*滚动条的轨道*/
-  :deep(.el-textarea__inner::-webkit-scrollbar-track) {
+  ::v-deep(.el-textarea__inner::-webkit-scrollbar-track) {
     background-color: #ffffff;
   }
   /*滚动条里面的小方块，能向上向下移动*/
-  :deep(.el-textarea__inner::-webkit-scrollbar-thumb) {
+  ::v-deep(.el-textarea__inner::-webkit-scrollbar-thumb) {
     background-color: rgba(144, 147, 153, 0.3);
     border-radius: 5px;
     border: 1px solid #f1f1f1;
     box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
   }
-  :deep(.el-textarea__inner::-webkit-scrollbar-thumb:hover) {
+  ::v-deep(.el-textarea__inner::-webkit-scrollbar-thumb:hover) {
     background-color: rgba(144, 147, 153, 0.3);
   }
   .el-textarea__inner::-webkit-scrollbar-thumb:active {
